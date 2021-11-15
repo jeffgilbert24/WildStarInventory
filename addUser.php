f141d938439250cff38bfa2b44251b1b9075358a
jeffgilbert24 <57629760+jeffgilbert24@users.noreply.github.com>
2021-11-14 17:55:14 -0800

    added connection file

---
+++ addUser.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Create</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css">
+         <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
+    <body>
+    <div class="container">  
+        <form action="php/create.php" method="POST">
+            <h4 class="display-4 text-center">Create</h4><hr><br>
+            <?php if (isset($_GET['error'])) { ?>           
+                <div class="alert alert-warning" role="alert">
+                    <?php echo $_GET['error']; ?>
+                </div>
+                <?php } ?>
+        <div class="mb-3">           
+            <input type="firstName" class="form-control" id="firstName"
+            name="firstName" placeholder="first name">
+        </div>
+
+        <div class="mb-3">            
+            <input type="lastName" class="form-control" id="lastName"
+            name="lastName" placeholder="last name">
+        </div>
+
+        <div class="mb-3">            
+            <input type="division" class="form-control" id="division" 
+            name="division"  placeholder="division">
+        </div>
+
+        <div class="mb-3">            
+            <input type="email" class="form-control" id="email" 
+            name="email"  placeholder="users email">
+        </div>
+        
+        <button type="submit" class="btn btn-primary"
+        name="create">Submit</button>
+        </form>
+
+
+    </div>
+  
+    </body>
+</html>
--- php/create.php
+++ php/create.php
@@ -11,34 +11,34 @@ if (isset($_POST['create'])) {
 
     $firstName = validate($_POST['firstName']);
     $lastName = validate($_POST['lastName']);
-    $userPhone = validate($_POST['userPhone']);
+    $department = validate($_POST['department']);
     $email = validate($_POST['email']);
 
-    $user_data = 'firstName=' .$firstName. 'lastName=' .$lastName. 'userPhone=' .$userPhone. 'email=' .$email;
+    $user_data = 'firstName=' .$firstName. 'lastName=' .$lastName. 'department=' .$department. 'email=' .$email;
 
     if (empty($firstName)) {
-        header("Location: ../index.php?error= first name is required&user_data");  
+        header("Location: ../addUser.php?error= first name is required&user_data");  
     }
     else
     if (empty($lastName)) {
-        header("Location: ../index.php?error= last name is required&user_data");
+        header("Location: ../addUser?error= last name is required&user_data");
     }
     else
-    if (empty($userPhone)) {
-        header("Location: ../index.php?error= phone number is required&user_data");
+    if (empty($department)) {
+        header("Location: ../addUser?error= department is required&user_data");
     }
     else
     if (empty($email)) {
-        header("Location: ../index.php?error= email is required&user_data");
+        header("Location: ../addUser?error= email is required&user_data");
     }
     else{
-        $sql = "INSERT INTO users(firstName, lastName, userPhone, email)
-                VALUES('$firstName', '$lastName', '$userPhone', '$email')";
+        $sql = "INSERT INTO users(firstName, lastName, department, email)
+                VALUES('$firstName', '$lastName', '$department', '$email')";
             $result = mysqli_query($conn, $sql);
             if ($result) {
                 echo "success";
             }else {
-                header("Location: ../index.php?error=unknown error occurred&user_data");
+                header("Location: ../addUser.php?error=unknown error occurred&user_data");
             }
     }
   
